74060ac43c24192fb9fb5c76621301c6398b7f9b
Rafatur Rahman Rakin <62703734+rafaturRakin@users.noreply.github.com>
2022-03-29 23:39:43 +0600

    Say "Hello, World!" With C++

---
+++ HackerRank/HelloWorld.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+    Author: Rakin
+    Date:  29-03-2022
+    Language: C++
+    OJ: HackerRank
+    Problem: [Say "Hello, World!" With C++]
+*/
+
+
+int main()
+{
+    cout << "Hello, World!";
+    return 0;
+}
