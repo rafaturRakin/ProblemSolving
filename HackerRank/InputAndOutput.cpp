9040b3742e5974fdbe76d3e5411b9d2b20f38bdb
Rafatur Rahman Rakin <62703734+rafaturRakin@users.noreply.github.com>
2022-03-29 23:58:40 +0600

    Input and Output

---
+++ HackerRank/InputAndOutput.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+    Author: Rakin
+    Date:  29-03-2022
+    Language: C++
+    OJ: HackerRank
+    Problem: [Input and Output]
+*/
+
+int main()
+{
+    int first, second, third, sum;
+    cin >> first >> second >> third;
+
+    sum = first + second + third;
+
+    cout << sum << endl;
+
+    return 0;
+}
