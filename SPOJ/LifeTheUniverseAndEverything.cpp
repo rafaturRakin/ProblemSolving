0967ffbb31a092adec5cc770fc9e6938357b44ba
Rafatur Rahman Rakin <62703734+rafaturRakin@users.noreply.github.com>
2022-03-30 23:54:40 +0600

    Life, the Universe, and Everything

---
+++ SPOJ/LifeTheUniverseAndEverything.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+    Author: Rakin
+    Date:  30-03-2022
+    Language: C++
+    OJ: SPOJ
+    Problem: [Life, the Universe, and Everything]
+*/
+
+int main()
+{
+    int number;
+    while(true)
+    {
+        cin >> number;
+        if(number == 42)
+            break;
+        cout << number << endl;
+    }
+    return 0;
+}
