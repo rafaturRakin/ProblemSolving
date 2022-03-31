efd4a834fcacf537bc5707e868cc7a9dd5f0efae
Rafatur Rahman Rakin <62703734+rafaturRakin@users.noreply.github.com>
2022-03-31 11:15:39 +0600

    Mangoes

---
+++ HackerEarth/Mangoes.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+    Author: Rakin
+    Date:  31-03-2022
+    Language: C++
+    OJ: HackerEarth
+    Problem: [Mangoes]
+*/
+
+int main()
+{
+    int number;
+
+    cin >> number;
+
+    if(number>2 && number%2==0)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+
+    return 0;
+}
