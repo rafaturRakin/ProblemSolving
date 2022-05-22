855d70c945ec79a148dfd45b1c404cdbe5fffa73
Rafatur Rahman Rakin <62703734+rafaturRakin@users.noreply.github.com>
2022-05-22 17:13:42 +0600

    Domino piling

---
+++ CodeForces/P-50-A.cpp
@@ -0,0 +1,22 @@
+/**
+    Name : Shah Rafatur Rahman Rakin
+    Dept. of CSE, HSTU, 2018
+
+    OJ : Code Forces
+    Problem : 50A
+    Date : 25-10-2020
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int m,n, result;
+    cin >> m >> n;
+    result = (m*n) / 2;
+    cout<< result << '\n';
+    return 0;
+}
