3f0eee3dd1d6d8b48cef3cd0992635b32d6adff9
Rafatur Rahman Rakin <62703734+rafaturRakin@users.noreply.github.com>
2022-05-31 21:36:12 +0600

    A. Word Capitalization

---
+++ CodeForces/P-281-A.cpp
@@ -0,0 +1,23 @@
+/**
+    Name : Shah Rafatur Rahman Rakin
+    Dept. of CSE, HSTU, 2018
+
+    OJ : Code Forces
+    Problem : 281A
+    Date : 27-10-2020
+*/
+
+#include<bits/stdc++.h>
+#define length 100
+
+using namespace std;
+int main()
+{
+    string word;
+    cin >> word;
+
+    word[0] = toupper(word[0]);
+
+    cout << word << '\n';
+    return 0;
+}
